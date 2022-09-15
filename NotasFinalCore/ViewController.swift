6f19262ab209ec679efa652c91da366765a6d9ea
isai Abraham <isaisabrahamf@gmail.com>
2022-09-15 12:40:18 -0500

    Initial Commit

---
+++ NotasFinalCore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NotasFinalCore
+//
+//  Created by Isai Abraham on 15/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
